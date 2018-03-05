69501797d591fa25290f04296ad0d3a8a3482a72
Taiga Nakayama <dora@dora-gt.jp>
2018-03-05 23:54:02 +0900

    bug

--- cs-ilp-codec-framework/Org/Interledger/Encoding/Asn/Codecs/AsnIA5StringBasedObjectCodecBase.cs
+++ cs-ilp-codec-framework/Org/Interledger/Encoding/Asn/Codecs/AsnIA5StringBasedObjectCodecBase.cs
@@ -3,7 +3,7 @@ namespace Org.Interledger.Encoding.Asn.Codecs
 {
     public abstract class AsnIA5StringBasedObjectCodecBase<T> : AsnCharStringBasedObjectCodecBase<T>
     {
-        public AsnIA5StringBasedObjectCodecBase(AsnSizeConstraint sizeConstraint) : base(sizeConstraint, System.Text.Encoding.GetEncoding("US_ASCII"))
+        public AsnIA5StringBasedObjectCodecBase(AsnSizeConstraint sizeConstraint) : base(sizeConstraint, System.Text.Encoding.GetEncoding("us-ascii"))
         {
         }
     }
