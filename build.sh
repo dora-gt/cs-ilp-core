4945ce1cff7f607312fd792a2de549c7b7b8462a
Taiga Nakayama <dora@dora-gt.jp>
2018-03-25 20:30:32 +0900

    fix

--- build.sh
+++ build.sh
@@ -71,7 +71,7 @@ if [ ! -f "$PACKAGES_CONFIG_MD5" ] || [ "$( cat "$PACKAGES_CONFIG_MD5" | sed 's/
     find . -type d ! -name . ! -name 'Cake.Bakery' | xargs rm -rf
 fi
 
-mono "$NUGET_EXE" install -ExcludeVersion
+mono "$NUGET_EXE" install -ExcludeVersion -Verbosity detailed
 if [ $? -ne 0 ]; then
     echo "Could not restore NuGet tools."
     exit 1
